d4edfa609f5ef76dd60161e88ddf742e5abc0c80
Akisa <akisa@cowsf.org>
2022-10-11 13:39:22 +0200

    Documented, check the commit for more info

--- main.go
+++ main.go
@@ -13,29 +13,41 @@ const (
 )
 
 func main() {
+  // Generates the seed using nanoseconds and current time
   s1 := rand.NewSource(time.Now().UnixNano())
   r1 := rand.New(s1)
+  // Init the counters
   headsCounter := 0;
   tailsCounter := 0;
-
+  // For loop which runs (constant) RUNTIME times
   for i := 0; i < RUNTIME; i++ { 
 
     x := r1.Intn(2)
+    // Checks if it's heads or tails
     if x == HEADS {
+      // Increments the counter if it's heads
       headsCounter++;
     } else if x == TAILS {
+      // Increments the counter if it's tails
       tailsCounter++;
     }
 
   }
-  
+  // Prints the counter 
   fmt.Println("Heads: ", headsCounter)
   fmt.Println("Tails: ", tailsCounter)
 
+  // Checks to see the difference and makes sure it's always a positive integer
   if(headsCounter > tailsCounter){
     fmt.Println("Difference: ", headsCounter - tailsCounter);
   }
+
   if(tailsCounter > headsCounter){
     fmt.Println("Difference: ", tailsCounter - headsCounter);
+  }
+
+  // This is nigh-impossible to get on higher amounts of coinflips
+  if(tailsCounter == headsCounter){
+    fmt.Println("Difference: None");
   }
 }
